c3e7357a9f0f2baa91d83cf464cd19a6a8086c8a
angre-kim <angre@daum.net>
2020-01-30 14:53:43 +0900

    screen UI

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,13 +5,23 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.lightBlueAccent,
-        body: Container(//safearea 대신 padding이 있는 container로 변경
+        body: Container(
+          //safearea 대신 padding이 있는 container로 변경
           padding:
-          EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom:30.0),
+              EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
           child: Column(
             children: <Widget>[
-              Icon(Icons.list),
-              Text('ToDoApp')],
+              CircleAvatar(
+                child: Icon(
+                  Icons.list,
+                  size: 30.0,
+                  color: Colors.lightBlueAccent,
+                ),
+                backgroundColor: Colors.white,
+                radius: 30.0,
+              ),
+              Text('ToDoApp')
+            ],
           ),
         ));
   }
