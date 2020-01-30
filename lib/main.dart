060652cb59fdaff024768bb33acd35806fd28e19
angre-kim <angre@daum.net>
2020-01-30 13:45:14 +0900

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp0130/screens/tasks_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: TasksScreen(),
+    );
   }
 }
