e5bb04ab9c2b38039ffa0aaa258664e1d958b7b5
angre-kim <angre@daum.net>
2020-01-30 15:50:51 +0900

    screen UI-하단 좌우 꽉 차게

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,53 +5,58 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        //safearea 대신 padding이 있는 container로 변경
-        padding:
-            EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            CircleAvatar(
-              child: Icon(
-                Icons.list,
-                size: 30.0,
-                color: Colors.lightBlueAccent,
-              ),
-              backgroundColor: Colors.white,
-              radius: 30.0,
+      body: Column(
+        children: <Widget>[
+          Container(
+            //safearea 대신 padding이 있는 container로 변경
+            padding:
+                EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 30.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'ToDoApp',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  'sdfsd',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+
+              ],
             ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              'ToDoApp',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 50.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Text(
-              'sdfsd',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18.0,
-              ),
-            ),
-            Container(
-              height: 300.0,
+          ),
+          Expanded(
+            child: Container(
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(20.0),
                   topRight: Radius.circular(20.0),
-
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
