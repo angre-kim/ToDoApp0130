45fe047739dc7c901eac70672c15ea1f51a88c2f
angre-kim <angre@daum.net>
2020-01-30 15:34:26 +0900

    screen UI

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -4,35 +4,48 @@ class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.lightBlueAccent,
-        body: Container(
-          //safearea 대신 padding이 있는 container로 변경
-          padding:
-              EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
-          child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                child: Icon(
-                  Icons.list,
-                  size: 30.0,
-                  color: Colors.lightBlueAccent,
-                ),
-                backgroundColor: Colors.white,
-                radius: 30.0,
+      backgroundColor: Colors.lightBlueAccent,
+      body: Container(
+        //safearea 대신 padding이 있는 container로 변경
+        padding:
+            EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            CircleAvatar(
+              child: Icon(
+                Icons.list,
+                size: 30.0,
+                color: Colors.lightBlueAccent,
               ),
-              SizedBox(
-                height: 10.0,
+              backgroundColor: Colors.white,
+              radius: 30.0,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'ToDoApp',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 50.0,
+                fontWeight: FontWeight.w700,
               ),
-              Text(
-                'ToDoApp',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 50.0,
-                  fontWeight: FontWeight.w700,
-                ),
-              )
-            ],
-          ),
-        ));
+            ),
+            Text(
+              'sdfsd',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ),
+            ),
+            Container(
+              color: Colors.white,
+                height: 300.0,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
