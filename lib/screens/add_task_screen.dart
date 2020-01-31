22cd7d6b331078c1187896a68ad04cf613a2356f
angre-kim <angre@daum.net>
2020-01-31 11:40:51 +0900

    199강-showbottomsheet container extract 후 UI2

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -3,6 +3,17 @@ import 'package:flutter/material.dart';
 class AddTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+     color: Color(0xff757575),//전체 색상을 흐리게 주고
+     child: Container(//각지게 만들기위해 하나 더 생성
+       decoration: BoxDecoration(
+         color: Colors.white,
+         borderRadius: BorderRadius.only(
+           topLeft: Radius.circular(20.0),
+           topRight: Radius.circular(20.0),
+         ),
+       ),
+     ),
+    );
   }
 }
