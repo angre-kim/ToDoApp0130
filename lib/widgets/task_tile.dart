a3f6c3b52f66269972079e1485eab214946c188b
angre-kim <angre@daum.net>
2020-01-31 14:02:45 +0900

    201강-checkbox 완성

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -16,18 +16,17 @@ class TaskCheckbox extends StatefulWidget {
 }
 
 class _TaskCheckboxState extends State<TaskCheckbox> {
-
-  bool isChecked = false;//2. 변수선언
+  bool isChecked = false; //2. 변수선언
 
   @override
   Widget build(BuildContext context) {
     return Checkbox(
       activeColor: Colors.lightBlue,
-      value: true,
-      onChanged: (newValue) {
-        isChecked = newValue;//3.
-
-      }, //1. newValue가 입력되면
+      value: isChecked, //4.
+      onChanged: (newValue) {        //1. newValue가 입력되면
+        setState(() {});  //5.
+        isChecked = newValue; //3.
+      },
     );
   }
 }
