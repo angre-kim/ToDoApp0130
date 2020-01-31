9911dcfd5e106270013b2b7d097f87153da2d183
angre-kim <angre@daum.net>
2020-01-31 12:48:05 +0900

    199강-showbottomsheet container extract 후 UI 3

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -4,16 +4,31 @@ class AddTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-     color: Color(0xff757575),//전체 색상을 흐리게 주고
-     child: Container(//각지게 만들기위해 하나 더 생성
-       decoration: BoxDecoration(
-         color: Colors.white,
-         borderRadius: BorderRadius.only(
-           topLeft: Radius.circular(20.0),
-           topRight: Radius.circular(20.0),
-         ),
-       ),
-     ),
+      color: Color(0xff757575), //전체 색상을 흐리게 주고
+      child: Container(
+        //좌우 각지게 만들기위해 하나 더 생성
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            Text('할일 추가',
+              style: TextStyle(
+                color: Colors.lightBlueAccent,
+                fontSize: 30.0,
+                ),
+            ),
+            TextField(
+              autofocus: true,
+            ),
+            FlatButton(),
+          ],
+        ),
+      ),
     );
   }
 }
