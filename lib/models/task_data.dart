414bb7a451fbd02867481613f18bd8fa1daca1dd
angre-kim <angre@daum.net>
2020-02-04 11:08:06 +0900

    209강-checking

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -4,15 +4,14 @@ import 'package:todoapp0130/models/task.dart';
 import 'dart:collection';
 
 class TaskData extends ChangeNotifier {
-
   List<Task> _tasks = [
     Task(name: '우유사기'),
     Task(name: '빵사기'),
     Task(name: '과자사기'),
   ];
-UnmodifiableListView<Task> get tasks {
-  return UnmodifiableListView(_tasks);
-}
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
 
   int get taskCount {
     return _tasks.length;
@@ -24,4 +23,8 @@ UnmodifiableListView<Task> get tasks {
     notifyListeners();
   }
 
+  void upDateTask(Task task){
+    task.toggleDone();
+    notifyListeners();
+  }
 }
